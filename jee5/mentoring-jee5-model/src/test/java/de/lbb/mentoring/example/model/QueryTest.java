7efe81c8d9597cd7669eca8cc45c187e0c7ad513
dbremer-tonn <daniel.bremer-tonn@akquinet.de>
2011-06-08 21:36:57 +0200

    added some interesting new Query Tests

--- jee5/mentoring-jee5-model/src/test/java/de/lbb/mentoring/example/model/QueryTest.java
+++ jee5/mentoring-jee5-model/src/test/java/de/lbb/mentoring/example/model/QueryTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import java.util.List;
 
 public class QueryTest extends AbstractEntityTest
 {
@@ -33,5 +34,59 @@ public class QueryTest extends AbstractEntityTest
         EmployeeVO emplVO = (EmployeeVO) query.getSingleResult();
         Assert.assertEquals(employee.getName(), emplVO.getName());
         Assert.assertEquals(employee.getSurename(), emplVO.getSurename());
+    }
+
+
+    /**
+     * Query with "null" in where clause
+     */
+    @Test
+    public void testNullTest()
+    {
+        Employee employee = new EmployeeBuilder().withAge(30).withName("Hans").withSurename("Mueller").build();
+
+        // saving Employee
+        em.getTransaction().begin();
+        em.persist(employee);
+        em.getTransaction().commit();
+
+        // loading Employee from new EM
+        EntityManager otherEM = emf.createEntityManager();
+        Employee reloadedEmployee = otherEM.find(Employee.class, employee.getId());
+        Assert.assertNull(reloadedEmployee.getDepartment());
+
+        Query query = otherEM.createQuery("select e from Employee e where e.department = :department");
+        query.setParameter("department", null);
+        List result = query.getResultList();
+
+        // result is empty, because comparing to null is always false
+        Assert.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testImplicitInnerJoin()
+    {
+        Employee employee = new EmployeeBuilder().withAge(30).withName("Hans").withSurename("Mueller").build();
+
+        // saving Employee
+        em.getTransaction().begin();
+        em.persist(employee);
+        em.getTransaction().commit();
+
+        EntityManager otherEM = emf.createEntityManager();
+
+        Query query = otherEM.createQuery("select e.department, e.name from Employee e");
+        List result = query.getResultList();
+
+        // result is empty, because e.department is creating an implizit inner-join, ommitting all
+        // Employees without an Department
+        Assert.assertTrue(result.isEmpty());
+
+        // Using an explicit Outer-Join to get the desired result
+        query = otherEM.createQuery("select d, e.name from Employee e left outer join e.department d");
+
+        result = query.getResultList();
+        // Thanks to the outer-join our Employee is finally found
+        Assert.assertFalse(result.isEmpty());
     }
 }
