43ec3f8ce07749aa07972bff04483a98d9c8bd7b
dbremer-tonn <daniel.bremer-tonn@akquinet.de>
2011-06-08 21:38:22 +0200

    moved bulkquerytest to querytest

--- jee5/mentoring-jee5-model/src/test/java/de/lbb/mentoring/example/model/QueryTest.java
+++ jee5/mentoring-jee5-model/src/test/java/de/lbb/mentoring/example/model/QueryTest.java
@@ -36,6 +36,36 @@ public class QueryTest extends AbstractEntityTest
         Assert.assertEquals(employee.getSurename(), emplVO.getSurename());
     }
 
+    /**
+     * Showing how bulk queries does not effect running entitymanager
+     */
+    @Test
+    public void testBulkQueries()
+    {
+        Employee employee = new EmployeeBuilder().withAge(30).withName("Hans").withSurename("Mueller").build();
+
+        // saving Employee
+        em.getTransaction().begin();
+        em.persist(employee);
+        em.getTransaction().commit();
+
+        em.getTransaction().begin();
+        Query query = em.createQuery("delete from Employee");
+        query.executeUpdate();
+        em.getTransaction().commit();
+
+        // Entitymanager does not know anything about the deleted Employee
+        Employee reloadedEmployee = em.find(Employee.class, employee.getId());
+        Assert.assertNotNull(reloadedEmployee);
+
+        // Detaching all entities
+        em.clear();
+
+        // After a clear, the Entitymanager is forced to reload the Employee
+        reloadedEmployee = em.find(Employee.class, employee.getId());
+        Assert.assertNull(reloadedEmployee);
+    }
+
 
     /**
      * Query with "null" in where clause
